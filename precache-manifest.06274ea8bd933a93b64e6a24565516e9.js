16b6a114b948ccf79bf14af4e25c598ece3fbc83
Konstantinos Bazakos <kbazakos@gmail.com>
2022-10-20 11:36:45 +0300

    Updates

---
+++ precache-manifest.06274ea8bd933a93b64e6a24565516e9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "46e6d38d002dc6fb7fe3",
+    "url": "/react-todo-app/static/css/main.11ed6d46.chunk.css"
+  },
+  {
+    "revision": "46e6d38d002dc6fb7fe3",
+    "url": "/react-todo-app/static/js/main.46e6d38d.chunk.js"
+  },
+  {
+    "revision": "a55b08022d704fb59a35",
+    "url": "/react-todo-app/static/js/1.a55b0802.chunk.js"
+  },
+  {
+    "revision": "dc884f14d85a35ea3dce",
+    "url": "/react-todo-app/static/js/runtime~main.dc884f14.js"
+  },
+  {
+    "revision": "78ff8828a61550bd7fa36a42bc3106b9",
+    "url": "/react-todo-app/index.html"
+  }
+];
