e442df29f26cd023bc502326aea27d077c2ad799
Konstantinos Bazakos <kbazakos@gmail.com>
2022-10-20 11:42:21 +0300

    Homepage gh bug

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ReactDOM from "react-dom"
-import {HashRouter as Router, BrowserRouter, Routes, Route } from "react-router-dom"
+import {BrowserRouter  as Router, BrowserRouter, Routes, Route } from "react-router-dom"
 import About from './pages/About'
 import NotMatch from './pages/NotMatch'
 import TodoContainer from "./functionBased/components/TodoContainer"
@@ -13,7 +13,7 @@ import TodoItemPage from "./functionBased/components/TodoItemPage"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Navbar />
       <Routes>
         <Route path="/" element={<TodoContainer />} />
